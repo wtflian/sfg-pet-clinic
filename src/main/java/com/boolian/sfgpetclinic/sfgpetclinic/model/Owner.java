8dd39295a9d022abff83bfee2cf503a1a9d4add8
boolian <lharutyunyan99@gmail.com>
2021-02-09 17:59:00 +0400

    Adding POJOs. Closes #2

---
+++ src/main/java/com/boolian/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.boolian.sfgpetclinic.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/boolian/sfgpetclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.boolian.sfgpetclinic.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
