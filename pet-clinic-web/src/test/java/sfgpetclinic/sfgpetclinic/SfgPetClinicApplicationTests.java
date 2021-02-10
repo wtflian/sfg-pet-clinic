119988fada0f75f9ba62417e686f6cded4c9b2f1
boolian <lharutyunyan99@gmail.com>
2021-02-10 17:55:44 +0400

    commit tests

--- pet-clinic-web/src/test/java/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,12 @@
 package sfgpetclinic.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class SfgPetClinicApplicationTests {
+@SpringBootTest(classes = SpringExtension.class)
+public class SfgPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
