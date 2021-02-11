36532f37a34627655f4c6317ae2bca8865027c04
boolian <lharutyunyan99@gmail.com>
2021-02-11 14:47:57 +0400

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/boolian/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,18 @@
+package com.boolian.sfgpetclinic.services;
+
+import com.boolian.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/boolian/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.boolian.sfgpetclinic.services;
+
+import com.boolian.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
